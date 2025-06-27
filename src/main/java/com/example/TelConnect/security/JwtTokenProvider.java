ea8d732137879d24331a0a65b8c98a3db7dc4e86
Haarish Anandan <haarish.anandan@gmail.com>
2025-06-28 01:06:31 +0530

    Added JWT auth utils files

---
+++ src/main/java/com/example/TelConnect/security/JwtTokenProvider.java
@@ -0,0 +1,57 @@
+package com.example.TelConnect.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Value;
+import org.springframework.security.core.Authentication;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+
+public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app-jwt-expiration-milliseconds}")
+    private long jwtExpirationDate;
+
+    public String generateToken(Authentication authentication){
+
+        String name = authentication.getName();
+        Date currentDate= new Date();
+
+        Date expireDate= new Date(currentDate.getTime() + jwtExpirationDate);
+
+        return Jwts.builder()
+                .setSubject(name)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getName(String token){
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token){
+        Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parse(token);
+        return true;
+    }
+}
