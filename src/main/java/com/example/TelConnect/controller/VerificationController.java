6c0f15517d8b93318b88ea202f82d823322e4d15
Haarish Anandan <haarish.anandan@gmail.com>
2025-08-08 13:17:13 +0530

    Updated user validation for verification endpoints

--- src/main/java/com/example/TelConnect/controller/VerificationController.java
+++ src/main/java/com/example/TelConnect/controller/VerificationController.java
@@ -1,8 +1,11 @@
 package com.example.TelConnect.controller;
 
 import com.example.TelConnect.DTO.VerificationRequestDTO;
+import com.example.TelConnect.model.Customer;
 import com.example.TelConnect.model.Verification;
+import com.example.TelConnect.service.CustomerService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,9 +21,11 @@ import java.util.Map;
 public class VerificationController {
 
     private final VerificationService verificationService;
+    private final CustomerService customerService;
 
-    public VerificationController(VerificationService verificationService){
+    public VerificationController(VerificationService verificationService, CustomerService customerService){
         this.verificationService=verificationService;
+        this.customerService=customerService;
     }
 
     //Handler to save verification attempt
@@ -32,7 +37,17 @@ public class VerificationController {
 
     //Handler to get verification status of customer
     @GetMapping("/{customerId}/status")
-    public ResponseEntity<?> getVerificationStatus(@PathVariable Long customerId){
+    public ResponseEntity<?> getVerificationStatus(@PathVariable Long customerId, HttpServletRequest request){
+        Customer customer= customerService.getByCustomerId(customerId);
+        String customerEmail= customer.getCustomerEmail();
+        String requesterEmail= request.getAttribute("userName").toString();
+
+        if(customer.getCustomerId()==1)
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+
+        if(!requesterEmail.equals(customerEmail))
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+
         Map<String, String> response= verificationService.getVerificationStatus(customerId);
         if(response.isEmpty())
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No verification request submitted by customer");
@@ -43,7 +58,17 @@ public class VerificationController {
 
     // Handler to update verification status
     @PatchMapping("/{customerId}/status")
-    public ResponseEntity<String> updateVerificationStatus(@PathVariable Long customerId, @RequestParam String status) {
+    public ResponseEntity<String> updateVerificationStatus(@PathVariable Long customerId, @RequestParam String status, HttpServletRequest request) {
+        Customer customer= customerService.getByCustomerId(customerId);
+        String customerEmail= customer.getCustomerEmail();
+        String requesterEmail= request.getAttribute("userName").toString();
+
+        if(customer.getCustomerId()==1)
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+
+        if(!requesterEmail.equals(customerEmail))
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+
         if(verificationService.updateVerificationStatus(customerId, status))
             return ResponseEntity.ok("Status updated successfully");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No verification request found");
